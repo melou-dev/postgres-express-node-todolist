5a6f1e53b8556a8dcf153a2e63aa5d23f14c114a
melou-dev <ameliecuaces.dev@gmail.com>
2019-12-30 13:56:23 +0100

    add express middleware and complete README into back-end

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
 
 const app = express();
+
 const port = 4000;
+
+// Log requests to the console.
+app.use(logger("dev"));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get("*", (req, res) =>
+  res.status(200).send({
+    message: "Welcome to the beginning of nothingness."
+  })
+);
 
 app.listen(port);
